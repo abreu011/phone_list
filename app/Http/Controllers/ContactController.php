dadd6ec444645c8c6be45b5fdaefb0d9c7d095fc
abreu011 <lucasabreu17@gmail.com>
2025-02-14 10:10:04 -0300

    Adiciona métodos para atualizar e deletar contato no ContactController. Atualiza rotas para as operações de PUT e DELETE.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ContactResource;
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ContactController extends Controller
 {
@@ -28,4 +29,22 @@ class ContactController extends Controller
 
         return new ContactResource($contact);
     }
+
+    public function update(Request $request, string $id) // Update contact by id
+    {
+        $contact = Contact::findOrFail($id);
+        $data = $request->all();
+        $contact->update($data);
+
+        return new ContactResource($contact);        
+    }
+
+    public function destroy(string $id) // Delete contact by id
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+
+        return response()->json([], Response::HTTP_NO_CONTENT); // Return status 204 (sucesso)     
+    }
+
 }
